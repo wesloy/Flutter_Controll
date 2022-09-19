004eeeb9e1921167fe557072c786519e5057af58
Wesley Eloy <52499714+wesloy@users.noreply.github.com>
2022-09-19 16:44:17 -0300

    Estruturando

---
+++ controll/lib/models/task.dart
@@ -0,0 +1,17 @@
+class Tasks {
+  final DateTime hoursWorked;
+  final int videos;
+  final int placements;
+  final int visits;
+  final int studies;
+  final DateTime date;
+
+  Tasks({
+    required this.hoursWorked,
+    required this.videos,
+    required this.placements,
+    required this.visits,
+    required this.studies,
+    required this.date,
+  });
+}
